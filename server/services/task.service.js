b48583fbd5636114f8236af0c8d2ffaeef890606
JeanVPacheco <jeanvitorp@gmail.com>
2022-02-15 19:58:31 -0300

    Adding service

---
+++ server/services/task.service.js
@@ -0,0 +1,15 @@
+const { taskModel } = require('../models');
+
+const taskSchema = require('../schemas/task');
+
+const createTask = async (title, description, status) => {
+  const { error } = taskSchema.validate({ title, description, status });
+  if (error) throw errorGenerator(400, error.message);
+
+  const newTask = await taskModel.createTask(title, description, status);
+  return newTask;
+};
+
+module.exports = {
+  createTask,
+};
