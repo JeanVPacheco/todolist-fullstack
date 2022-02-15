7101fe7caeade3a2ddcd13c7514bfb898816a392
JeanVPacheco <jeanvitorp@gmail.com>
2022-02-15 19:53:30 -0300

    Adding task schema

--- server/models/task.model.js
+++ server/models/task.model.js
@@ -1,11 +1,11 @@
 const connect = require('../db');
 
-const createTask = async (userId, title, description, status) => {
+const createTask = async (title, description, status) => {
   const conn = await connect();
 
   const newTask = await conn.collection('tasks')
     .insertOne({
-      userId, title, description, status,
+      title, description, status,
     });
 
   return newTask;
